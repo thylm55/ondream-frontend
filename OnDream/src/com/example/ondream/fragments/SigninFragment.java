0b3ad931d465b40dc3c32e89bf7860f36b68ecda
tuyendev <tuyendev@gmail.com>
2014-03-22 13:21:09 +0700

    w
    updated

--- OnDream/src/com/example/ondream/fragments/SigninFragment.java
+++ OnDream/src/com/example/ondream/fragments/SigninFragment.java
@@ -1,33 +1,25 @@
 package com.example.ondream.fragments;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.android.volley.Response.Listener;
 import com.example.ondream.DataParsingController;
 import com.example.ondream.OnDreamVolley;
 import com.example.ondream.R;
-import com.example.ondream.models.MComment;
 import com.example.ondream.models.MDream;
 
-public class SigninFragment extends BaseFragment 
+public class SigninFragment extends BaseFragment implements OnClickListener 
 {
 	Button btnSignin;
 	EditText edtEmail;
@@ -42,13 +34,20 @@ public class SigninFragment extends BaseFragment
 		btnSignin = (Button) view.findViewById(R.id.btn_Signin);
 		edtEmail = (EditText) view.findViewById(R.id.edt_Email);
 		edtPasswd = (EditText) view.findViewById(R.id.edt_Passwd);
-		btnSignin.setOnClickListener(new OnClickListener()
-		{
-			@Override
-			public void onClick(View v){
-				
-			}
-		});
+		btnSignin.setOnClickListener(this);
 		return view;
+	}
+	
+	@Override
+	public void onClick(View v) {
+		if (v == btnSignin) {
+			OnDreamVolley.getLocalClient().postLogin(edtEmail.getText().toString(), edtPasswd.getText().toString(), new Listener<JSONObject>() {
+
+				@Override
+				public void onResponse(JSONObject arg0) {
+					
+				}
+			}, getErrorListener());
+		}
 	}
 }
